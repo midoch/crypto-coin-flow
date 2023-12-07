6433d7ce722925084f6fa3eb63dbe4ae96b33dc8
mido <101448145+midoch@users.noreply.github.com>
2023-12-07 23:05:59 +0100

    Create project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-roboto text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
