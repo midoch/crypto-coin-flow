61b8068015237ece98fab9c949627c6588104fd2
mido <101448145+midoch@users.noreply.github.com>
2023-12-08 15:34:08 +0100

    Changed text

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -6,10 +6,10 @@ const ErrorBoundary = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <div className="text-white p-8 rounded-lg shadow-md bg-gray-800">
+      <div className="text-white p-4 rounded-lg shadow-md bg-gray-800">
         <h1 className="text-4xl font-bold text-red-500 mb-4">Oops!</h1>
-        <p className="text-lg text-gray-300 mb-4">
-          Sorry, an unexpected error has occurred.
+        <p className="text-md text-gray-300 mb-4">
+          Sorry, What you are looking for is not here.
         </p>
         <p className="italic text-gray-400 mb-4">
           {error.statusText || error.message}
