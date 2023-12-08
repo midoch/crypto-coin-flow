ef18155190dff7ccb8bd1f5bc30ba0df61ed722e
mido <101448145+midoch@users.noreply.github.com>
2023-12-08 18:24:43 +0100

    Created HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from "react";
-
-export default function Home() {
+const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-start text-white">
+      <div className="text-center mt-10">
+        <h1 className="text-4xl font-bold text-blue-500 mb-4">
+          Welcome to Crypto Coin Flow
+        </h1>
+        <p className="text-gray-300 text-lg mb-8">
+          Explore the world of cryptocurrencies and stay updated with real-time
+          market data.
+        </p>
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
