007182fc8f71da45028d302502d9db9d872a6626
mido <101448145+midoch@users.noreply.github.com>
2023-12-11 22:00:30 +0100

    Fixed bug on mobile view

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -6,16 +6,21 @@ import { Link } from "react-router-dom";
 const Coins = (props) => {
   return (
     <div className="container mx-auto px-4 py-8 bg-gray-900 text-white">
-      <div className="grid grid-cols-6 gap-4 font-bold mb-4">
+      <div className="grid grid-cols-6 gap-4 font-bold mb-4 pl-4">
         <div className="col">#</div>
         <div className="col">Coin</div>
-        <div className="col">Price</div>
+        <div className="col sm:hidden pl-12">Price</div>
         <div className="col hidden md:block">24h Change</div>
         <div className="col hidden md:block">Volume</div>
         <div className="col hidden md:block">Market Cap</div>
       </div>
       {props.coins.map((coins) => (
-        <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id}>
+        <Link
+          to={`/coin/${coins.id}`}
+          element={<Coin />}
+          key={coins.id}
+          className="cursor-pointer"
+        >
           <div className="grid grid-cols-6 gap-4 bg-gray-800 p-4 rounded-md shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 items-center mb-4">
             <div className="col">{coins.market_cap_rank}</div>
             <div className="col flex items-center space-x-2">
@@ -26,7 +31,9 @@ const Coins = (props) => {
               />
               <p className="text-sm">{coins.symbol.toUpperCase()}</p>
             </div>
-            <div className="col">${coins.current_price.toLocaleString()}</div>
+            <div className="col sm:hidden pl-12">
+              ${coins.current_price.toLocaleString()}
+            </div>
             <div className="col hidden md:block">
               {coins.price_change_percentage_24h.toFixed(2)}%
             </div>
