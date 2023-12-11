0bb1c5a3a41eab61b49b8b244d8d4c31b8a020ea
mido <101448145+midoch@users.noreply.github.com>
2023-12-11 19:23:13 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,34 +5,47 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const url =
+  const API_URL =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en";
 
   useEffect(() => {
-    axios
-      .get(url)
-      .then((res) => {
-        setCoins(res.data);
-        console.log(res.data[0]);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        setCoins(response.data);
+      } catch (error) {
+        setError("Please try again later.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [API_URL]);
 
   return (
     <>
       <Navbar />
-      <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-start text-white">
+      <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-start text-white">
         <div className="text-center mt-10">
-          <h1 className="text-4xl font-bold text-blue-500 mb-4">
-            Welcome to Crypto Coin Flows
+          <h1 className="text-5xl font-bold text-teal-500 mb-4">
+            Discover Crypto Coin Flows
           </h1>
           <p className="text-gray-300 text-lg mb-8">
-            Explore the world of cryptocurrencies and stay updated with
-            real-time market data.
+            Dive into the realm of cryptocurrencies and keep pace with real-time
+            market data.
           </p>
         </div>
-        <Coins coins={coins} />
+        {loading ? (
+          <p className="text-gray-300 text-xl">Loading...</p>
+        ) : error ? (
+          <p className="text-red-500 text-xl">Error: {error}</p>
+        ) : (
+          <Coins coins={coins} />
+        )}
       </div>
     </>
   );
