195c569321e888454429be79e759dbdd0ccc67eb
mido <101448145+midoch@users.noreply.github.com>
2023-12-11 21:43:17 +0100

    Update CoinItem.jsx

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const CoinItem = (props) => {
   return (
-    <div className="bg-gray-800 p-4 rounded-md shadow-md mb-4">
-      <p className="text-lg font-bold text-green-500">
+    <div className="bg-gray-800 p-6 rounded-md shadow-md mb-4">
+      <p className="text-lg font-bold text-green-500 mb-2">
         Rank #{props.coins.market_cap_rank}
       </p>
-      <div className="flex items-center mb-2">
-        <img src={props.coins.image} alt="crypto" className="w-8 h-8 mr-2" />
+      <div className="flex items-center mb-4">
+        <img src={props.coins.image} alt="crypto" className="w-10 h-10 mr-2" />
         <p className="text-xl font-semibold text-white">
           {props.coins.symbol.toUpperCase()}
         </p>
@@ -20,7 +20,7 @@ const CoinItem = (props) => {
           props.coins.price_change_percentage_24h >= 0
             ? "text-green-500"
             : "text-red-500"
-        }`}
+        } mb-2`}
       >
         24h Change: {props.coins.price_change_percentage_24h.toFixed(2)}%
       </p>
