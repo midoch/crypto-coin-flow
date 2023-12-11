c62ff747276842aafe0056ecb30e9df836771b60
mido <101448145+midoch@users.noreply.github.com>
2023-12-11 19:23:35 +0100

    Changed styling

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -2,16 +2,34 @@ import React from "react";
 
 const CoinItem = (props) => {
   return (
-    <div>
-      <p>{props.coins.market_cap_rank}</p>
-      <div>
-        <img src={props.coins.image} alt="crypto" />
-        <p>{props.coins.symbol.toUpperCase()}</p>
+    <div className="bg-gray-800 p-4 rounded-md shadow-md mb-4">
+      <p className="text-lg font-bold text-green-500">
+        Rank #{props.coins.market_cap_rank}
+      </p>
+      <div className="flex items-center mb-2">
+        <img src={props.coins.image} alt="crypto" className="w-8 h-8 mr-2" />
+        <p className="text-xl font-semibold text-white">
+          {props.coins.symbol.toUpperCase()}
+        </p>
       </div>
-      <p>${props.coins.current_price.toLocaleString()}</p>
-      <p>{props.coins.price_change_percentage_24h.toFixed(2)}%</p>
-      <p>${props.coins.total_volume.toLocaleString()}</p>
-      <p>${props.coins.market_cap.toLocaleString()}</p>
+      <p className="text-lg font-semibold text-white">
+        Price: ${props.coins.current_price.toLocaleString()}
+      </p>
+      <p
+        className={`text-lg ${
+          props.coins.price_change_percentage_24h >= 0
+            ? "text-green-500"
+            : "text-red-500"
+        }`}
+      >
+        24h Change: {props.coins.price_change_percentage_24h.toFixed(2)}%
+      </p>
+      <p className="text-lg font-semibold text-white">
+        Volume: ${props.coins.total_volume.toLocaleString()}
+      </p>
+      <p className="text-lg font-semibold text-white">
+        Market Cap: ${props.coins.market_cap.toLocaleString()}
+      </p>
     </div>
   );
 };
--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,22 +1,42 @@
+// Coins.jsx
 import React from "react";
 import CoinItem from "./CoinItem";
-import Coin from "../routes/Coin";
 import { Link } from "react-router-dom";
 
 const Coins = (props) => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-6 gap-2 font-bold text-gray-800">
-        <div>#</div>
-        <div>Coin</div>
-        <div>Price</div>
-        <div>24h</div>
-        <div>Volume</div>
-        <div>Market Cap</div>
+    <div className="container mx-auto px-4 py-8 bg-gray-900 text-white">
+      <div className="grid grid-cols-6 gap-4 font-bold mb-4">
+        <div className="col">#</div>
+        <div className="col">Coin</div>
+        <div className="col">Price</div>
+        <div className="col hidden md:block">24h Change</div>
+        <div className="col hidden md:block">Volume</div>
+        <div className="col hidden md:block">Market Cap</div>
       </div>
       {props.coins.map((coins) => (
-        <Link to={`/coin/${coins.id}`} key={coins.id}>
-          <CoinItem coins={coins} />
+        <Link to={`/coins/${coins.id}`} key={coins.id}>
+          <div className="grid grid-cols-6 gap-4 bg-gray-800 p-4 rounded-md shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 items-center mb-4">
+            <div className="col">{coins.market_cap_rank}</div>
+            <div className="col flex items-center space-x-2">
+              <img
+                src={coins.image}
+                alt="crypto"
+                className="w-8 h-8 mr-2 rounded-full"
+              />
+              <p className="text-sm">{coins.symbol.toUpperCase()}</p>
+            </div>
+            <div className="col">${coins.current_price.toLocaleString()}</div>
+            <div className="col hidden md:block">
+              {coins.price_change_percentage_24h.toFixed(2)}%
+            </div>
+            <div className="col hidden md:block">
+              ${coins.total_volume.toLocaleString()}
+            </div>
+            <div className="col hidden md:block">
+              ${coins.market_cap.toLocaleString()}
+            </div>
+          </div>
         </Link>
       ))}
     </div>
