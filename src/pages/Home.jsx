7d8c3831e494e05c5fead05418b8d86efe93244b
mido <101448145+midoch@users.noreply.github.com>
2023-12-11 23:07:54 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   const [error, setError] = useState(null);
 
   const API_URL =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en";
 
   useEffect(() => {
     const fetchData = async () => {
